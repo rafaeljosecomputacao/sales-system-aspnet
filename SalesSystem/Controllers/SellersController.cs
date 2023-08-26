978105ac86a96716f965e2add8d7e45f3cff4814
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-26 04:04:44 -0300

    sellers-controller

---
+++ SalesSystem/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesSystem.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
