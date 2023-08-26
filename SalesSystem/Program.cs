18565eabbbcc872165d7c443ce7830248cbdc1c0
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-26 20:52:35 -0300

    foreign-key-not-null-referential-integrity

--- SalesSystem/Program.cs
+++ SalesSystem/Program.cs
@@ -27,7 +27,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-if (app.Environment.IsDevelopment())
+else
 {
     app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
 }
--- SalesSystem/Services/SellerService.cs
+++ SalesSystem/Services/SellerService.cs
@@ -20,6 +20,7 @@ namespace SalesSystem.Services
 
         public void Insert(Seller seller)
         {
+            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
