440d73ede0f47791f1e825bb3f08cb96de1bfd5d
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2023-08-29 05:47:17 -0300

    exception-handling-for-delete-referential-integrity

---
+++ SalesSystem/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesSystem.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message) { }
+    }
+}
